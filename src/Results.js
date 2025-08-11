c9ead87f805d85473c6dd27721c68bb59e7dd435
Sophie-Ndobe <139694211+Sophie-Ndobe@users.noreply.github.com>
2025-08-11 16:54:59 +0200

    updated Results component

--- src/Results.js
+++ src/Results.js
@@ -4,7 +4,7 @@ export default function Results(props) {
   console.log(props.results);
   return (
     <div className="Results">
-      <h2>{props.results.word}</h2>
+      <h2>Word</h2>
       <p>These are the results from the API call</p>
     </div>
   );
