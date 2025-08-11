586650c084c4f4487d177b01c8725b91dd2f64e1
Sophie-Ndobe <139694211+Sophie-Ndobe@users.noreply.github.com>
2025-08-11 15:19:19 +0200

    Added a footer to the project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,35 @@ function App() {
     <div className="App">
       <div className="container">
         <h1 className="mt-3">My Dictionary App</h1>
+        <footer>
+          <p>
+            Authored by{" "}
+            <a
+              href="https://github.com/Sophie-Ndobe?tab=repositories"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <strong>Nkateko Ndobe</strong>
+            </a>
+            , this{" "}
+            <a
+              href="https://github.com/Sophie-Ndobe/dictionary-ndobe-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-source{" "}
+            </a>
+            project is deployed and hosted on{" "}
+            <a
+              href="https://dictionary-words-check.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Netlify
+            </a>
+            .
+          </p>
+        </footer>
       </div>
     </div>
   );
