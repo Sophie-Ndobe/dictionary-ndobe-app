1afa348b5f24cd6b20dd400cf752f779e7e54af0
Sophie-Ndobe <139694211+Sophie-Ndobe@users.noreply.github.com>
2025-08-11 15:59:57 +0200

    Added the API intergration to the Dictionary component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState(" ");
+
+  function displayDefinitions(response) {
+    console.log(response);
+  }
+
   function submitSearch(event) {
     event.preventDefault();
-    alert("Searching");
+    alert(`Searching for the ${keyword}`);
+
+    let apiKey = "2c13e0a2b6fe347b0421bb02eef2o43t";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(displayDefinitions);
   }
 
   function displaySearch(event) {
-    console.log(event.target.value);
+    setKeyword(event.target.value);
   }
 
   return (
